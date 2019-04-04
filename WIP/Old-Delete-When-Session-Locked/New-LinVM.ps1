2fc456bab03717fbcf1f2b0e1c6b9757526c95b8
mibender <mibender@microsoft.com>
2019-04-04 16:03:58 -0500

    Added files from Thomas

---
+++ WIP/Old-Delete-When-Session-Locked/New-LinVM.ps1
@@ -0,0 +1,20 @@
+$RGName = 'mibenderdemo'
+$location = 'westus'
+
+$subnetConfig = New-AzVirtualNetworkSubnetConfig -Name "mibenderSubnet" -AddressPrefix 192.168.1.0/24
+$vnet = New-AzVirtualNetwork -ResourceGroupName $RGName -Location $location -Name 'mibenderVNET' -AddressPrefix 192.168.0.0/16 -Subnet $subnetConfig
+$pip = New-AzPublicIpAddress -ResourceGroupName $RGName -Location $location -AllocationMethod Static -IdleTimeoutInMinutes 4 -Name "mibenderPublicDNS$(Get-Random)"
+
+$nsgRuleSSH = New-AzNetworkSecurityRuleConfig -Name "mibenderNetworkSecurityGroupRuleSSH" -Protocol "TCP" -Direction "Inbound" -Priority 1000 -SourceAddressPrefix * -SourcePortRange * -DestinationAddressPrefix * -DestinationPortRange 22 -Access 'Allow'
+$nsgRuleWeb = New-AzNetworkSecurityRuleConfig -Name "mibenderNetworkSecurityGroupRuleWWW" -Protocol "TCP" -Direction "Inbound" -Priority 1001 -SourceAddressPrefix * -SourcePortRange * -DestinationAddressPrefix * -DestinationPortRange 80 -Access 'Allow'
+$nsg = New-AzNetworkSecurityGroup -ResourceGroupName $RGName -Location $location -Name 'mibenderNSG' -SecurityRules $nsgRuleSSH,$nsgRuleWeb
+
+$nic = New-AzNetworkInterface -Name "mibenderNic" -ResourceGroupName $RGName -Location $location -SubnetId $vnet.Subnets[0].Id -PublicIpAddressId $pip.Id -NetworkSecurityGroupId $nsg.Id
+
+$vmConfig = New-AzVMConfig -VMName 'DemoLin' -VMSize "Standard_D1" | Set-AzureRMVMOperatingSystem -Linux -ComputerName 'DemoLin' -Credential $cred -DisablePasswordAuthentication | Set-AzureRMVMSourceImage -PublisherName "Canonical" -Offer "UbuntuServer" -Skus "16.04-LTS" -Version "latest" | Add-AzureRMVMNetworkInterface -Id $nic.Id
+
+$sshPublicKey = Get-Content "/home/michael/.ssh/id_rsa.pub"
+Add-AzVMSshPublicKey -VM $vmConfig -KeyData $sshPublicKey -Path "/home/michael/.ssh/authorized_keys"
+
+
+New-AzVM -ResourceGroupName $RGName -Location westus -VM $vmConfig
